fec9c0b1be0b127a2985ba771b418ba7e0d1a931
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-10-30 23:16:16 +0900

    test: tweet作成機能のテストを実装

--- tweets/tests.py
+++ tweets/tests.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
+from tweets.models import Tweet
+
 User = get_user_model()
 
 
@@ -19,14 +21,63 @@ class TestHomeView(TestCase):
         self.assertTemplateUsed(response, "tweets/home.html")
 
 
-# class TestTweetCreateView(TestCase):
-#     def test_success_get(self):
+class TestTweetCreateView(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser",
+            email="testuser@example.com",
+            password="asdf!@#$1234",
+        )
+        self.url = reverse("tweets:create")
 
-#     def test_success_post(self):
+    def test_success_get(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.url)
 
-#     def test_failure_post_with_empty_content(self):
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "tweets/create.html")
 
-#     def test_failure_post_with_too_long_content(self):
+    def test_success_post(self):
+        self.client.force_login(self.user)
+
+        valid_data = {"body": "Tweet"}
+        response = self.client.post(self.url, valid_data)
+
+        self.assertRedirects(
+            response,
+            reverse("tweets:home"),
+            status_code=302,
+            target_status_code=200,
+        )
+        self.assertTrue(Tweet.objects.filter(user=self.user).filter(body=valid_data["body"]).exists())
+
+    def test_failure_post_with_empty_content(self):
+        self.client.force_login(self.user)
+
+        invalid_data = {"body": ""}
+        response = self.client.post(self.url, invalid_data)
+
+        self.assertContains(
+            response,
+            "このフィールドは必須です",
+            count=1,
+            status_code=200,
+        )
+        self.assertEqual(len(Tweet.objects.all()), 0)
+
+    def test_failure_post_with_too_long_content(self):
+        self.client.force_login(self.user)
+
+        invalid_data = {"body": "T" * 141}
+        response = self.client.post(self.url, invalid_data)
+
+        self.assertContains(
+            response,
+            "この値は 140 文字以下でなければなりません( 141 文字になっています)。",
+            count=1,
+            status_code=200,
+        )
+        self.assertEqual(len(Tweet.objects.all()), 0)
 
 
 # class TestTweetDetailView(TestCase):
