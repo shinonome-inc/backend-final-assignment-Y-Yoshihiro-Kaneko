3756344fa55592265b41e3d6dde685b98f05eb2a
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-10-30 21:31:16 +0900

    feat: Tweetモデルを作成

--- tweets/models.py
+++ tweets/models.py
@@ -1,4 +1,13 @@
-# from django.db import models
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
 
 
-# class Tweet(models.Model):
+class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    body = models.TextField(max_length=140)
+
+    def __str__(self):
+        return f"{self.user.username}'s post"
