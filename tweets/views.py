4e673641aa1bc70f2512ce5fa1efb94f35de05fb
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-10-30 22:31:49 +0900

    feat: tweet作成機能を実装

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,24 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
+
+from tweets.models import Tweet
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = "tweets/home.html"
+
+
+class TweetCreateView(LoginRequiredMixin, CreateView):
+    template_name = "tweets/create.html"
+    model = Tweet
+    fields = ("body",)
+    success_url = reverse_lazy("tweets:home")
+
+    def form_valid(self, form):
+        tweet = form.save(commit=False)
+        tweet.user = self.request.user
+        tweet.save()
+        self.object = tweet
+        return HttpResponseRedirect(self.get_success_url())
