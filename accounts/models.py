294a5b03194fff512fb8fa14046ab3cbe7aea8b8
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-10-31 18:58:19 +0900

    feat: フォロー関係を表す FriendShip モデルを作成

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,27 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 
 
 class User(AbstractUser):
     email = models.EmailField()
+    following = models.ManyToManyField(
+        "self",
+        related_name="followers",
+        symmetrical=False,
+        through="FriendShip",
+        through_fields=("follower", "followee"),
+    )
 
     def get_absolute_url(self):
         return reverse("accounts:user_profile", kwargs={"username": self.username})
 
 
-# class FriendShip(models.Model):
+class FriendShip(models.Model):
+    # フォローしている人
+    follower = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="+", on_delete=models.CASCADE)
+    # フォローされている人
+    followee = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="+", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
