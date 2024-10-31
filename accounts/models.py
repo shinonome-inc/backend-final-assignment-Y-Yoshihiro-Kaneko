d3058e12e4bccdcb05bf566814b66656c4c350cb
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-10-31 17:44:10 +0900

    feat: サイトの構造を使いやすいように変更

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
 
 class User(AbstractUser):
     email = models.EmailField()
+
+    def get_absolute_url(self):
+        return reverse("accounts:user_profile", kwargs={"username": self.username})
 
 
 # class FriendShip(models.Model):
--- tweets/models.py
+++ tweets/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
 
 User = get_user_model()
 
@@ -11,3 +12,6 @@ class Tweet(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s post"
+
+    def get_absolute_url(self):
+        return reverse("tweets:detail", kwargs={"pk": self.pk})
