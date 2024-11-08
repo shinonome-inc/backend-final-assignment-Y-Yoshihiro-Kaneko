9c1a56791fabc3cb9fbc4688c750150bc4ea8717
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-11-09 02:16:22 +0900

    chore: django-debug-toolbar の設定

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import include, path
 
@@ -22,4 +23,4 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("tweets/", include("tweets.urls")),
     path("", include("welcome.urls")),
-]
+] + debug_toolbar_urls()
