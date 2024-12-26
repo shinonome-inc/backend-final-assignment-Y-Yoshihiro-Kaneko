56bab9b19cfee6243d607f441e5e6904f2fbec1e
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-12-26 18:49:17 +0900

    refactor: SQLを一回で実行するように変更
    `tweet` と `tweet.user` の計2回SQLを実行していたが、
    `select_related("user")` で1回の実行に合体させた

--- tweets/views.py
+++ tweets/views.py
@@ -29,7 +29,7 @@ class TweetCreateView(LoginRequiredMixin, CreateView):
 
 class TweetDetailView(LoginRequiredMixin, DetailView):
     template_name = "tweets/detail.html"
-    model = Tweet
+    queryset = Tweet.objects.select_related("user")
 
 
 class TweetDeleteView(UserPassesTestMixin, DeleteView):
